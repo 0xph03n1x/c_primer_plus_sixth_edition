e401b64eb9a417cbf72f16fe9a90bd63261678f3
0xph03n1x <lazarovmihail97@gmail.com>
2023-08-26 02:06:45 +0200

    Chapter 10

---
+++ Chapter_10/order.c
@@ -0,0 +1,17 @@
+/* order.c == precedence in pointer operations */
+#include <stdio.h>
+
+int data[2] = {100,200};
+int moderate[2] = {300,400};
+
+int main(void)
+{
+    int *p1, *p2, *p3;
+    p1 = p2 = data;
+    p3 = moderate;
+    printf(" *p1 = %d, *p2 = %d, *p3 = %d\n", *p1 , *p2 , *p3);
+    printf("*p1++ = %d, *++p2 = %d, (*p3)++ = %d\n", *p1++ , *++p2 , (*p3)++);
+    printf(" *p1 = %d, *p2 = %d, *p3 = %d\n", *p1 , *p2 , *p3);
+
+    return 0;
+}
---
+++ Chapter_10/zippo2.c
@@ -0,0 +1,6 @@
+/* zippo2.c == zippo info via a pointer variable */
+#include <stdio.h>
+
+int main(void)
+{
+}
